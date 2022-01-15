7244214cf5290f041e0eaffc876666d42c7290a7
meXuvo <cse.engr.shuvo@gmail.com>
2022-01-15 21:38:17 +0600

    check version program and user info program

---
+++ UserInfo.c
@@ -0,0 +1,9 @@
+// Write a C program to print your name, date of birth.and mobile number.
+#include <stdio.h>
+int main()
+{
+    printf("Name         :Rafiqul Islam\n");
+    printf("Birth        :1996.02.07\n");
+    printf("mobile number:01792166627\n");
+    return 0;
+}
